04b6bd3d40a3f5bacabdd4931d64c84955daaf9d
sumenkovdv <sumenkov.dv@yandex.ru>
2022-10-12 09:34:16 +0700

    описание

--- src/main/java/ru/sumenkov/mysqltojson/repository/TableOperation.java
+++ src/main/java/ru/sumenkov/mysqltojson/repository/TableOperation.java
@@ -3,8 +3,8 @@ package ru.sumenkov.mysqltojson.repository;
 import java.sql.SQLException;
 
 public interface TableOperation {
-    void readTable() throws SQLException;
-    void getOneDay() throws SQLException;
-    void getMount() throws SQLException;
-    void getPeriod() throws SQLException;
+    void readTable() throws SQLException; // SELECT * FROM '2022'
+    void getOneDay() throws SQLException; // SELECT * FROM '2022' WHERE dt1='2022-07-20'
+    void getMonth() throws SQLException; // SELECT * FROM '2022' WHERE dt1 LIKE '2022-07-%'
+    void getDatePeriod() throws SQLException; // SELECT * FROM '2022' WHERE dt1 BETWEEN '2022-07-20' and '2022-07-31'
 }
