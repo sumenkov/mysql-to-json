dc8f92770575f06e23097d60ac6ec67ca7e84c4d
sumenkovdv <sumenkov.dv@yandex.ru>
2022-10-12 08:38:13 +0700

    описание

--- src/main/java/ru/sumenkov/mysqltojson/repository/TableOperation.java
+++ src/main/java/ru/sumenkov/mysqltojson/repository/TableOperation.java
@@ -4,4 +4,7 @@ import java.sql.SQLException;
 
 public interface TableOperation {
     void readTable() throws SQLException;
+    void getOneDay() throws SQLException;
+    void getMount() throws SQLException;
+    void getPeriod() throws SQLException;
 }
