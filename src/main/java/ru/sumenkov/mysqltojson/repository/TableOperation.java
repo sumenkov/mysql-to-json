d15b1854ac4c78191a17786c0b2855c64a47f3df
sumenkovdv <sumenkov.dv@yandex.ru>
2022-10-14 15:49:50 +0700

    описание

--- src/main/java/ru/sumenkov/mysqltojson/repository/TableOperation.java
+++ src/main/java/ru/sumenkov/mysqltojson/repository/TableOperation.java
@@ -1,12 +1,22 @@
 package ru.sumenkov.mysqltojson.repository;
 
-import ru.sumenkov.mysqltojson.model.TableSQLModel;
-
 import java.sql.SQLException;
+import java.util.Date;
 
 public interface TableOperation {
-    void readTable() throws SQLException; // SELECT * FROM '2022'
-    void getOneDay() throws SQLException; // SELECT * FROM '2022' WHERE dt1='2022-07-20'
-    void getMonth() throws SQLException; // SELECT * FROM '2022' WHERE dt1 LIKE '2022-07-%'
-    void getDatePeriod() throws SQLException; // SELECT * FROM '2022' WHERE dt1 BETWEEN '2022-07-20' and '2022-07-31'
+    void readTable(int year) throws SQLException; // SELECT * FROM '2022'
+    void readTable(int year, int numberOfLines) throws SQLException; // SELECT * FROM '2022' LIMIT 0, 1000
+    void readTable(int year, int firstLine, int numberOfLines) throws SQLException; // SELECT * FROM '2022' LIMIT 10, 1000
+
+    void readOneDay(Date date) throws SQLException; // SELECT * FROM '2022' WHERE dt1='2022-07-20'
+    void readOneDay(Date date, int numberOfLines) throws SQLException; // SELECT * FROM '2022' WHERE dt1='2022-07-20' LIMIT 0, 1000
+    void readOneDay(Date date, int firstLine, int numberOfLines) throws SQLException; // SELECT * FROM '2022' WHERE dt1='2022-07-20' LIMIT 10, 1000
+
+    void readOneMonth(Date date) throws SQLException; // SELECT * FROM '2022' WHERE dt1 LIKE '2022-07-%'
+    void readOneMonth(Date date, int numberOfLines) throws SQLException; // SELECT * FROM '2022' WHERE dt1 LIKE '2022-07-%' LIMIT 0, 1000
+    void readOneMonth(Date date, int firstLine, int numberOfLines) throws SQLException; // SELECT * FROM '2022' WHERE dt1 LIKE '2022-07-%' LIMIT 10, 1000
+
+    void readDatePeriod(Date dateStart, Date dateEnd) throws SQLException; // SELECT * FROM '2022' WHERE dt1 BETWEEN '2022-07-20' and '2022-07-31'
+    void readDatePeriod(Date dateStart, Date dateEnd, int numberOfLines) throws SQLException; // SELECT * FROM '2022' WHERE dt1 BETWEEN '2022-07-20' and '2022-07-31' LIMIT 0, 1000
+    void readDatePeriod(Date dateStart, Date dateEnd, int firstLine, int numberOfLines) throws SQLException; // SELECT * FROM '2022' WHERE dt1 BETWEEN '2022-07-20' and '2022-07-31' LIMIT 10, 1000
 }
